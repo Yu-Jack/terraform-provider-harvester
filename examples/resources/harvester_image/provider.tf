218504e74d3c380ec9425860872e3feec4e5b706
Jack Yu <jack.yu@suse.com>
2025-07-04 16:50:41 +0800

    Test
    Signed-off-by: Jack Yu <jack.yu@suse.com>

---
+++ examples/resources/harvester_image/provider.tf
@@ -0,0 +1,8 @@
+terraform {
+  required_providers {
+    harvester = {
+      source = "harvester/harvester"
+      version = "0.6.7" /* replace version you'd like to test*/
+    }
+  }
+}
---
+++ internal/tests/image_test.go
@@ -0,0 +1,25 @@
+package tests
+
+import (
+	"testing"
+
+	"github.com/gruntwork-io/terratest/modules/terraform"
+)
+
+func TestTerraform_normal(t *testing.T) {
+	t.Parallel()
+
+	// Construct the terraform options with default retryable errors to handle the most common
+	// retryable errors in terraform testing.
+	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
+		// The path to where our Terraform code is located
+		TerraformDir: "../../examples/resources/harvester_image",
+	})
+
+	// At the end of the test, run `terraform destroy` to clean up any resources that were created.
+	defer terraform.Destroy(t, terraformOptions)
+
+	// Run `terraform init` and `terraform apply`. Fail the test if there are any errors.
+	terraform.InitAndApply(t, terraformOptions)
+
+}
